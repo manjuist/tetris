f51a43d93294c8b273e644b09b9d81b8ad85c083
deve <deve_m@163.com>
2015-12-21 16:33:49 +0800

    gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -3,15 +3,71 @@ module.exports = function(grunt){
 		pkg:grunt.file.readJSON('package.json'),
 		uglify:{
 			options:{
-				banner:''
+				banner:'/*! <%=pkg.name%>-<%=pkg.version%> <%=grunt.template.today("yyyy-mm-dd")%> */\n'
 			},
 			build:{
-				src:'',
-				dest:''
+				src:'js/els.js',
+				dest:'build/<%=pkg.name%>-<%=pkg.version%>.js.min.js'
+			}
+		},
+		jshint:{
+			build:['Gruntfile.js','js/*.js'],
+			options:{
+				jshintrc:".jshintrc"
+			}
+		},
+		sass:{
+			build:{
+				files:[{
+					expand:true,
+					src:['css/*.scss'],
+					dest:'build/',
+					ext:'.css'
+				}]
+			}
+		},
+		csslint:{
+			build:['build/css/*.css'],
+			options:{
+				csslintrc:".csslintrc"
+			}
+		},
+		postcss:{
+			options:{
+				map:true,
+				processors:[require('autoprefixer')({
+					browsers:['last 2 versions']
+				})]
+			},
+			dist:{
+				src:'build/css/*.css'
+			}
+		},
+		watch:{
+			build:{
+				files:['js/*.js','css/*.scss'],
+				tasks:['uglify','jshint','sass','csslint','postcss:dist'],
+				options:{spawn:false}
+			}
+		},
+		browserSync:{
+			bsFiles:{
+				src:['css/*.scss','*.html','js/*.js']
+			},
+			options:{
+				server:{
+					baseDir:'./'
+				}
 			}
 		}
 	});
 
-	grunt.loadNpmTasks('');
-	grunt.registerTask('',[]);
-}
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.registerTask('default',['browserSync','watch']);
+};
