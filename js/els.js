c1b1c29ccbac75c260176e7057dc841056e26fe1
deve <deve_m@163.com>
2015-12-21 16:33:21 +0800

    els.js

--- js/els.js
+++ js/els.js
@@ -8,11 +8,11 @@ els.ele = [[0x6600],[0x2222,0xf00],[0xc600,0x2640],[0x6c00,0x4620],[0x4460,0x2e0
 els.random = function(min,max){
 	var len = Array.prototype.slice.call(arguments).length,
 		num = (function(len){
-			_num = {
+			var _num = {
 				'0':[0,1],
 				'1':[0,min],
 				'2':[min,max]
-			}
+			};
 			return _num[len];
 		}(len));
 	return parseInt(Math.random()*(num[1]-num[0]+1)+num[0]);
@@ -21,7 +21,7 @@ els.random = function(min,max){
 
 //颜色
 els.color = (function(){
-	color = ['#0ff','#f0f','#ff0','#00f','#0f0','#f00','#000'];
+	var color = ['#0ff','#f0f','#ff0','#00f','#0f0','#f00','#000'];
 	return color[els.random(6)];
 }());
 //颜色结束
@@ -34,7 +34,7 @@ els.shape = function(arr){
 	for(var i = 0; i < len; i++){
 		var sublen = arr[i].length;
 		for(var j = 0; j < len; j++){
-			if(arr[i][j] != 0){
+			if(arr[i][j] !== 0){
 				var oldHtml = document.getElementById('gameing').innerHTML;
 				document.getElementById('gameing').innerHTML = oldHtml + '<div class="current" style="width: 30px; height: 30px; position: absolute; top: '+i*30+'px; left: '+j*30+'px;background:'+color+';"></div>';
 			}
@@ -70,39 +70,42 @@ var eventUtil = {
 var get = function(selector){
 	var first = selector.slice(0,1),
 		_selector = selector.slice(1);
-	if(first == '#'){
+	if(first === '#'){
 		return document.getElementById(_selector);
-	}else if(first == '.'){
+	}else if(first === '.'){
 		if(document.getElementsByClassName){
 			return document.getElementsByClassName(_selector);
 		}else{
+			return;
 		}
 	}else{
+		return;
 	}
 };
 //选择器结束
 
 //移动函数
 els.move = function(ele,orient){
-	if(!ele instanceof Array){
+	if(!(ele instanceof Array)){
 		var tem = [];
 		tem.push(ele);
 		ele = tem;
 	}
 	for(var i in ele){
-		if((typeof ele[i]).toLowerCase() == 'object'){
-		};
+		if((typeof ele[i]).toLowerCase() === 'object'){
+			return;
+		}
 	}
 	function _right(_ele){
-		_ele.style.left = parseInt(document.defaultView?document.defaultView.getComputedStyle(_ele).left:_ele.currentStyle['left']) + 30 + 'px';
-	};
+		_ele.style.left = parseInt(document.defaultView?document.defaultView.getComputedStyle(_ele).left:_ele.currentStyle.left) + 30 + 'px';
+	}
 	function _bottom(_ele){
-		_ele.style.top = parseInt(document.defaultView?document.defaultView.getComputedStyle(_ele).top:_ele.currentStyle['top']) + 30 + 'px';
-	};
+		_ele.style.top = parseInt(document.defaultView?document.defaultView.getComputedStyle(_ele).top:_ele.currentStyle.top) + 30 + 'px';
+	}
 	function _left(_ele){
-		_ele.style.left = parseInt(document.defaultView?document.defaultView.getComputedStyle(_ele).left:_ele.currentStyle['left']) - 30 + 'px';
-	};
-}
+		_ele.style.left = parseInt(document.defaultView?document.defaultView.getComputedStyle(_ele).left:_ele.currentStyle.left) - 30 + 'px';
+	}
+};
 //移动函数结束
 els.shape(els.ele[els.random(3)][els.random(1,4)]);
 
