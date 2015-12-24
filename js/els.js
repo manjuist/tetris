d75688e01aeccdeccbaef78bab364ee3e659ac4b
deve <deve_m@163.com>
2015-12-24 18:50:56 +0800

    els.js

--- js/els.js
+++ js/els.js
@@ -1,7 +1,7 @@
 function els(){}
 
 //生成随机数
-els.random = function(min,max){
+var random = function(min,max){
 	//获取参数个数
 	var len = Array.prototype.slice.call(arguments).length,
 		//根据参数个数决定随机数取值范围
@@ -18,13 +18,13 @@ els.random = function(min,max){
 };
 
 //颜色
-els.color = function(){
+var color = function(){
 	var color = ['#0ff','#f0f','#ff0','#00f','#0f0','#f00','#000'];
-	return color[els.random(6)];
+	return color[random(6)];
 };
 
 //事件
-els.eventUtil = {
+var eventUtil = {
 	addHandler:function(element,type,handler){
 		if(element.addEventListener){
 			element.addEventListener(type,handler,false);
@@ -45,39 +45,46 @@ els.eventUtil = {
 	}
 };
 
-els.eventUtil.addHandler(window,'keydown',function(e){
+eventUtil.addHandler(window,'keydown',function(e){
 	var event = e || event;
-	return event.keyCode;
+	switch(event.keyCode){
+		case 37:{
+			_shape.shape.left();
+			break;
+		}
+		case 39:{
+			_shape.shape.right();
+			break;
+		}
+		case 40:{
+			_shape.shape.down(5);
+			break;
+		}
+	}
 });
 
 //形状数组
-els.ele = [
-	[[0,0],[1,0],[0,1],[1,1]],
-	[[0,0],[0,1],[0,2],[0,3]],
-	[[1,0],[2,0],[0,1],[1,1]],
-	[[0,0],[1,0],[1,1],[2,1]],
-	[[0,0],[1,0],[2,0],[2,1]],
-	[[0,0],[1,0],[1,1],[1,2]],
-	[[1,0],[0,1],[1,1],[1,2]]
-];
+var ele = [ [[0,0],[1,0],[0,1],[1,1]], [[0,0],[0,1],[0,2],[0,3]], [[1,0],[2,0],[0,1],[1,1]], [[0,0],[1,0],[1,1],[2,1]], [[0,0],[1,0],[2,0],[2,1]], [[0,0],[1,0],[1,1],[1,2]], [[1,0],[0,1],[1,1],[1,2]] ];
 
 //形状
 var Shape = function(){
 	//随机获取形状
-	this.shape = els.ele[els.random(6)];
-	this.color = els.color();
+	this.shape = ele[random(6)];
+	this.color = color();
 };
 	Shape.prototype.self = function(){
 		return this;
 	};
-	Shape.prototype.down = function(){
+	Shape.prototype.down = function(val){
+		var val = val || 1;
 		if(this.bottomBorder()){
 			for(var i = 0; i < 4; i++){
-				this.shape[i][1] += 1;
+				this.shape[i][1] += val;
 			}
 			show.render(show.matrix(this.self()));
 		}else{
 			clearInterval(timer);
+			init();
 		}
 	};
 	Shape.prototype.left = function(){
@@ -108,8 +115,10 @@ var Shape = function(){
 		var cx = Math.round((this.shape[0][0] + this.shape[1][0] + this.shape[2][0] + this.shape[3][0])/4);
 		var cy = Math.round((this.shape[0][1] + this.shape[1][1] + this.shape[2][1] + this.shape[3][1])/4);
 		for(var i = 0; i < 4; i++){
-			this.shape[i][1] = cx - this.shape[i][1] + cy;
-			this.shape[i][0] = cy + this.shape[i][0] - cx;
+			var x = this.shape[i][0];
+			var y = this.shape[i][1];
+			this.shape[i][0] = cx - y + cy;
+			this.shape[i][1] = cy + x - cx;
 		}
 		show.render(show.matrix(this.self()));
 	};
@@ -129,18 +138,20 @@ var show = {
 	},
 	matrix:function(shape){
 		var temp = [];
-		//矩阵
-		for(var i = 0; i < 24; i++){
-			temp.push(new Array(16));
-		}
-		if(shape){
-			//将形状插入矩阵
-			for(var j = 0; j < 4; j++){
-				temp[shape.shape[j][1]][shape.shape[j][0]+7] = 1;
+		//return function(shape){
+			//矩阵
+			for(var i = 0; i < 24; i++){
+				temp.push(new Array(16));
 			}
-		}
-		//返回包含矩阵和形状的对象
-		return {matrix:temp,shape:shape || {}};
+			if(shape){
+				//将形状插入矩阵
+				for(var j = 0; j < 4; j++){
+					temp[shape.shape[j][1]][shape.shape[j][0]+7] = 1;
+				}
+			}
+			//返回包含矩阵和形状的对象
+			return {matrix:temp,shape:shape || {}};
+		//}
 	},
 	render:function(matrix){
 		var box = document.getElementById('js-game');
@@ -158,8 +169,13 @@ var show = {
 	}
 };
 
-var _shape = show.render(show.matrix(new Shape()));
-var timer = setInterval(function(){
-	_shape.shape.down();
-},1000);
+var _shape;
+var timer;
+function init(){
+	_shape = show.render(show.matrix(new Shape()));
+	timer = setInterval(function(){
+		_shape.shape.down();
+	},1000);
+}
+init();
 
