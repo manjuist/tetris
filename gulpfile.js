90bddc990be38cd8071ad9a9b76c5f68ca6ccc3a
deve <deve_m@163.com>
2018-01-23 22:02:01 +0800

    plugin

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,6 @@ var webpack = require('webpack');
 var del = require("del");
 var gutil = require("gulp-util");
 var webpack_config = require('./webpack.config.js');
-var autoprefixer = require('autoprefixer');
-var cssgrace = require('cssgrace');
-var cssnext = require('cssnext');
-var sass = require('gulp-sass');
-var pug = require('gulp-pug');
-var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var baseConfig = {
@@ -20,7 +14,7 @@ var baseConfig = {
 	dist:'dist/'
 };
 
-gulp.task('default',['clean','webpack','pug','browser-sync']);
+gulp.task('default',['clean','webpack','browser-sync']);
 
 gulp.task('browser-sync',function(){
 	//static
@@ -29,10 +23,6 @@ gulp.task('browser-sync',function(){
 			baseDir:"dist/"
 		}
 	});
-	// // proxy
-	// browserSync.init({
-	// 	proxy:"dev.mlamp.co"
-	// });
 	gulp.watch(baseConfig.src.js,['webpack']);
 	gulp.watch(baseConfig.src.html,['pug']);
 	gulp.watch(baseConfig.dist+'js/*.js').on('change',reload);
@@ -51,12 +41,6 @@ gulp.task('webpack',function(callback){
 		}));
 		callback();
 	});
-});
-
-gulp.task('pug',function(){
-	return gulp.src(baseConfig.src.html)
-		.pipe(pug())
-		.pipe(gulp.dest(baseConfig.dist));
 });
 
 gulp.task('watch',function(){
